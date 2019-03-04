f822387a5e71142edea38d21d6751f4cd142f874
danielkvist <d94.zaragoza@gmail.com>
2019-03-04 21:05:17 +0100

    init commit

---
+++ cmd/root.go
@@ -0,0 +1,65 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	homedir "github.com/mitchellh/go-homedir"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var (
+	cfgFile  string
+	dstRoute string
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "todots",
+	Short: "todots is a very simple CLI that helps you to easily have a copy of all of your dotfiles",
+	Long: `todots is a very simple CLI that helps you to easily have a copy of all of your dotfiles.
+	
+It basically copies in the directory that you specify all the files or directories that you have defined in the .todots file on your Home directory.`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		if dstRoute == "" {
+			return fmt.Errorf("No destination route provided")
+		}
+
+		fmt.Println(viper.AllKeys())
+		return nil
+	},
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.todots.yaml)")
+	rootCmd.PersistentFlags().StringVar(&dstRoute, "dst", "", "destination route")
+}
+
+func initConfig() {
+	if cfgFile != "" {
+		viper.SetConfigFile(cfgFile)
+	} else {
+		home, err := homedir.Dir()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		viper.AddConfigPath(home)
+		viper.SetConfigName(".todots")
+	}
+
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	}
+}
